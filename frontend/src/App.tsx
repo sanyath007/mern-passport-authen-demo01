d2ffa1e60b1edaf6a7d06ea2452a5e646c176692
sanyath007 <sanyath007@gmail.com>
2020-12-28 20:55:53 +0700

    Add code of each components and pages and add all of these to App.tsx with using react-router-dom components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AdminPage from './pages/AdminPage';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div>
+    <Router>
+      <Navbar />
 
-    </div>
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/admin' exact component={AdminPage} />
+        <Route path='/login' exact component={Login} />
+        <Route path='/profile' exact component={Profile} />
+      </Switch>
+    </Router>
   );
 }
 
